4c7fb69837c8f55f914f7d3e1838df7fb473bedf
Ela Kanani <65859031+Ela-Kan@users.noreply.github.com>
2024-10-27 15:31:33 +0000

    updating simulations

--- dictionaryMatching/dictionaryConversion.py
+++ dictionaryMatching/dictionaryConversion.py
@@ -17,9 +17,10 @@ import os
 import glob
 
 import time
+print(os.getcwd())
 
 t0 = time.time()
-
+import itertools
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -28,10 +29,10 @@ warnings.filterwarnings("ignore")
 
 
 ''' -----------------------------INPUTS--------------------------------- '''
-acqlen = 2000
+acqlen = 1000
 
 # Dictionary folder
-dictfolder = 'Test'
+dictfolder = 'FISP_WEX_ISMRM'
 
 #Type of normalisation performed 
 #Set to L2-norm (standard)
@@ -42,8 +43,46 @@ norm_type = 2 #1 #2
 print("Starting Dictionary Read in:  " + str(time.strftime('%X %x %Z')))
 
 #Number of entries in dictionary (to set array sizes)
-dictPath = './MRFSGRE_BBB/Dictionaries/Dictionary' + dictfolder + '/'
-no_entries = np.size( [f for f in os.listdir(dictPath) if f.endswith('.npy')])
+dictPath = '../dictionaries/Dictionary' + dictfolder + '/'
+
+##### comment out if not filtering dictionary#################
+resArray = range(200,1700,100) #range(200,1700,107) #range(200,1700,70) 
+# percentage blood volume (perc) UNIT: %
+percArray = range(10,110,10) #REMEMBER IT WILL BE DIVIDED BY 10 110
+#T1 of tissue compartment (t1t) UNIT: ms
+t1tArray = range(1000,2200,200) #range(700,1700,69) 
+#T1 of blood compartment (t1b) UNIT: ms
+t1bArray = range(1500,2100,200) #range(1540,1940,27) 
+# multiplication factor for the B1 value (multi)
+multiArray = range(80, 130, 10) #100
+# T2 of tissue compartment UNIT: ms
+t2tArray = range(38,127,15)
+# T2 of blood compartment UNIT: ms
+t2bArray = range(55, 385, 110)
+if t2tArray[-1] > 112:
+    t2tArray= list(t2tArray)
+    t2tArray[-1] = 112
+if percArray[-1] > 100:
+    percArray= list(percArray)
+    percArray[-1] = 100
+params = list(itertools.product(t1tArray, t1bArray, t2tArray, t2bArray, resArray, percArray, multiArray))
+# find mean T1 tissue and T1 blood
+t1tmean = np.mean(t1tArray)
+t1bmean = np.mean(t1bArray)
+t2tmean = np.mean(t2tArray)
+t2bmean = np.mean(t2bArray)
+
+# remove params from the search space:
+# if T1 tissue is above the mean, T2 tissue below the mean should be removed from list of params in itertools
+filtered_combinations = [
+    (t1t, t1b, t2t, t2b, res, perc, multi) for t1t, t1b, t2t, t2b, res, perc, multi in params
+    if not (t1t > t1tmean and t2t < t2tmean) and not(t1b > t1bmean and t2b < t2bmean) and not (t2t > t2tmean and t1t < t1tmean) and not (t2b > t2bmean and t1b < t1bmean) 
+]
+##########################
+
+#no_entries = np.size( [f for f in os.listdir(dictPath) if f.endswith('.npy')])
+no_entries = len(filtered_combinations)
+
 
 #Open empy arrays for dictionary signals
 array = np.zeros((acqlen,no_entries))
@@ -53,24 +92,34 @@ files = []
 count = 0
 sample = [0,0]
 rr = '*.npy' #rr = '*1.0_1.npy'
+convert_to_mag = True # whether to convert to a magnitude signal from complex
 #Loading all dictionary signals into array    
-for filename in glob.glob(os.path.join(str(dictPath + rr))):
+#for filename in glob.glob(os.path.join(str(dictPath + rr))):
+for param in filtered_combinations:
+    filename = f'../dictionaries/DictionaryFISP_WEX_ISMRM/echo_{param[0]}_{param[1]}_{param[2]}_{param[3]}_{param[4]}_{param[5]/10}_{param[6]/100}_1.npy'
     print(f"Processing file: {count+1}")
-    
     with open(os.path.join(filename), 'r') as f:
         #No water exchange variation considered
         #filesplit = filename.split('_')
         #if filesplit[3] == '300':
-        hold = np.load(filename)
+        if convert_to_mag == False:
+            hold = np.load(filename)
+        else:
+            hold_complex = np.load(filename)
+            hold = np.sqrt((hold_complex[:,0])**2 + (hold_complex[:,1])**2) # take the magnitude of the data
+        
         #Divided by the number of isochromats to give a fractional value
         try:
             # Save dictionary signals into array
-            array[:,count] = np.squeeze(hold[0:acqlen,0]) 
+            if convert_to_mag == False:
+                array[:,count] = np.squeeze(hold[0:acqlen,0]) 
+            else:
+                array[:,count] = hold[0:acqlen]
             #Save parameter values  (Look up table)
-            strsplit = filename.split('_')   
-            files.append([float(strsplit[2]),float(strsplit[3]), # change this depending on the file structure (print the filename to see what it looks like)
-                            float(strsplit[4]),float(strsplit[5]),
-                            float(strsplit[6])])
+            strsplit = filename.split('_')  
+            files.append([float(strsplit[3]),float(strsplit[4]), # change this depending on the file structure (print the filename to see what it looks like)
+                            float(strsplit[5]),float(strsplit[6]),
+                            float(strsplit[7]), float(strsplit[8]), float(strsplit[9])])
             count += 1
         except: 
             #Skip if it doesnt work 
--- dictionaryMatching/simulationMatching.py
+++ dictionaryMatching/simulationMatching.py
@@ -16,128 +16,253 @@ from scipy.stats import iqr
 from scipy.stats import entropy
 from sklearn.metrics import mutual_info_score
 from sklearn.preprocessing import MinMaxScaler
+import itertools
+import multiprocessing as mp
+import time
+import platform
 
 import warnings
 warnings.filterwarnings("ignore")
 
 scaler = MinMaxScaler()
 
-#go up a folder
-os.chdir("..")
-print(os.getcwd())
-
-"""-----------------------------FUNCTIONS----------------------------------"""
- 
-def calc_MI(x, y, bins):
-    c_xy = np.histogram2d(x, y, bins)[0]
-    mi = mutual_info_score(None, None, contingency=c_xy) 
-    #mi = normalized_mutual_info_score(list(x.T),list(y.T))
-    nmi = mi /(entropy(x)*entropy(y))
-    return nmi
-
-#FOR NORMALISING DENSITY ARRAY
-def normalized(a, axis=-1, order=2):
-    l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
-    l2[l2==0] = 1
-    return a / np.expand_dims(l2, axis)
 
 
-"""----------------------------INPUTS---------------------------------"""
+"""----------------------HELPER FUNCTIONS-----------------------------"""
+def load_dictionary(dictionaryid, acqlen):
+    print('Loading dictionary...')
+    #fPath = os.path.join(os.getcwd(), 'dictionaries', 'Dictionary' + dictionaryid)
+    #files = glob.glob(os.path.join(fPath, rr))
+    fPath = os.path.join(os.getcwd(), 'dictionaries', 'Dictionary' + dictionaryid + '/dictionary.txt')
+    no_entries = 94500 #len(files)
+    raw_dictionary = np.genfromtxt(fPath, delimiter=',', dtype=None, encoding=None).T
+    dictionary = np.zeros((acqlen, no_entries))
+    #for count, filename in enumerate(files): # normalise the dictionary
+    for i in range(no_entries):
+        dictionary[:, i] = np.squeeze(raw_dictionary[:, i] / np.linalg.norm(raw_dictionary[:, i]))
+        #dictionary[:, count] = np.squeeze(raw_dict[:acqlen] / np.linalg.norm(raw_dict[:acqlen]))
+    print('Dictionary loaded')
 
-#number of noise levels used in generation
-noi = 10
-#number of samples at each noise level used in generation
-samples = 50
-#length of signal
-acqlen = 500
-#size of the dictionary - used for setting array size, can overestimate
-no_entrys = 150
-#number of dimensions in the dictioanry
-parameters = 5
-
-#Name of the dictioanry
-dictionaryid = '2Dsim'
-sampleid = dictionaryid
-
-#load in dictionary 
-#Load files
-array = np.zeros((acqlen,no_entrys))
-files = []
-
-count = 0
-sample = [0,0]
-#rr = '*' +  str(repeat) + '.npy'
-rr = '*_1.npy'
-#Loading all signals
-
-#for filename in glob.glob(os.path.join(str('/Users/emmathomson/Desktop/Local/Dictionaries/Dictionary' + dictionaryid + '/' + rr))):
-for filename in glob.glob((str('./MRFSGRE_BBB/dictionaries/Dictionary' + dictionaryid + '/' + rr))):
-#for filename in glob.glob(os.path.join(str('/Volumes/MyPassportforMac/Dictionaries_2022/Dictionaries/Dictionary' + dictionaryid + '/' + rr))):    
-        with open(os.path.join(filename), 'r') as f:
-            hold = np.load(filename)
-            array[:,count] = np.squeeze(hold[:acqlen,0]/np.linalg.norm(hold[:acqlen,0]))
-            files.append(filename)
-
-        count += 1
-
-
-res_ave = np.zeros([noi,parameters])
-res_std = np.zeros([noi,parameters])
-res_perc = np.zeros([noi,parameters])
-
-#matching
-for per in range(10,105,10): #range(5,105,5): #range(1,11,1): 
-    for res in range(200,1700,100): #range(200,1700,100): #100000000000
-        for T1t in [1300]: #range(600,1600,100): #range(1100,1550,50):
-            for T1b in [1600]: #range(1500,2100,200):
-                for multi in [100]: #range(80, 120, 10): #range(80, 125, 5):
-                    res_est = np.zeros([parameters,samples])                                  
-                    for noise in range(0,noi):
-                            #for samp in range(0,samples):      
-                            samp=49
-                            correct_array = []
-                            ind =[]
-                            
-                            #perc = sam[int(per)]
-                            perc = per/10
-                        
-                            #Load the signal to match 
-                            test_name = './Dictionaries/Dictionary' + sampleid + '/echo_' + str(T1t) + '_' + str(T1b) + '_' + str(res) + '_' + str(perc) + '_' + str(multi/100) + '_' + str(samp+1) + '.npy' 
-
-                            hold =  np.load(test_name)
-                            test_signal = hold[:acqlen,noise]
-                            test_signal = test_signal/np.linalg.norm(test_signal)
-                        
-                            #test_signal = np.expand_dims(test_signal, axis=0)
-                            #Calculate the dot product 
-                            dot_sum = np.matmul(test_signal,array[:acqlen,:])
-
-
-                            #Find max index 
-                            dot_sum = dot_sum[dot_sum != 0]
-                            max_index = np.argmax(dot_sum)
-                            ind.append(max_index)
-                            match_name = files[max_index]
-                            strsplit = match_name.split('_')
-                            try:
-                                res_est[:,samp] = [float(strsplit[2]),float(strsplit[3]), float(strsplit[4]), float(strsplit[5]), float(strsplit[6])] #[:-4] 
-                            except: 
-                                res_est[:,samp] = [float(strsplit[1]),float(strsplit[2]), float(strsplit[3]), float(strsplit[4]), float(strsplit[5])] #[:-4] 
-                   
-                        #mode_calc, count = mode(res_est[:,:-1], axis=1)
-                        #res_ave[noise,:] = np.squeeze(mode_calc)
-                        res_ave[noise,:] = np.median(res_est[:,1:], axis=1)
-                        res_std[noise,:] = iqr(res_est[:,1:], axis=1)
-                    #res_perc[noise,:] = (np.mean(res_est, axis=1)-res)/res*100
-                    
-                    print('Input signal')
-                    print(test_name)  
-                    print ('Matched:')
-                    print(str(res_ave[0,0]), str(res_ave[0,1]),
-                          str(res_ave[0,2]), str(res_ave[0,3]), str(res_ave[0,4]))
-                
-                    pc = [res_ave, res_std]
-                
-                    file_name = './Dictionaries/SimulationMatching/Matching'+ sampleid + '/' + str(T1t) + '_' + str(T1b) + '_' + str(res) + '_' + str(perc) + '_' + str(multi/100) + '_pc.npy'
+    lPath = lPath = os.path.join(os.getcwd(), 'dictionaries', 'Dictionary' + dictionaryid, 'lookupTable.txt')
+    lookup=np.loadtxt(lPath,delimiter=',', dtype=None, encoding=None) # shape = no_entries,no_params
     
-                    np.save(file_name, pc)
+    return dictionary,lookup #, files
+
+
+def match_signal(test_name, dictionary, acqlen, noise, samples):
+    hold = np.load(test_name)
+    test_signal = hold[:acqlen, noise,:samples]
+    # Normalise the test signal
+    test_signal = test_signal / np.linalg.norm(test_signal)
+    #Calculate the dot product 
+    dot_sum = np.matmul(test_signal.T, dictionary[:acqlen, :])
+    #dot_sum = dot_sum[dot_sum != 0]
+    max_index = np.argmax(dot_sum, axis=1)
+    return max_index # return the index of the best dictionary match
+
+def process_matching(params):
+    T1t, T1b, T2t, T2b, res, per, multi, res_ave,res_std, res_est, sampleid, dictionary, lookup, acqlen, samples = params
+    for noise in range(res_ave.shape[0]): # number of noise levels is in the res_ave shape
+        samp = 50
+        perc = per/10
+        test_name = './simulationData/noisy_ISMRM' + '/echo_' + str(T1t) + '_' + str(T1b) + '_' + str(T2t)+ '_' + str(T2b) + '_' +  str(res) + '_' + str(perc) + '_' + str(multi/100) + '_' + str(samp) + '.npy' 
+        max_index = match_signal(test_name, dictionary, acqlen, noise, samples)
+        matched_signal= lookup[max_index,:]
+        res_est[:, :samp] = matched_signal.T
+        res_ave[noise, :] = np.median(res_est[:, 1:], axis=1)
+        res_std[noise, :] = iqr(res_est[:, 1:], axis=1)
+    
+    # Save results
+    pc = [res_ave, res_std]
+    file_name = f'./simulationData/noisy_ISMRM_matching/{T1t}_{T1b}_{T2t}_{T2b}_{res}_{perc}_{multi/100}_pc.npy'
+    np.save(file_name, pc)
+    # print results
+    """
+    print('Input signal')
+    print(test_name)  
+    print ('Matched:')
+    print(str(res_ave[0,0]), str(res_ave[0,1]),
+        str(res_ave[0,2]), str(res_ave[0,3]), str(res_ave[0,4]), str(res_ave[0,5]),str(res_ave[0,6]))
+    """
+    return None
+
+if __name__ == "__main__":
+
+    #go up a folder
+    os.chdir("..")
+    print(os.getcwd())
+
+    """----------------------------INPUTS---------------------------------"""
+    #number of noise levels used in generation
+    noi = 2
+    #number of samples at each noise level used in generation
+    samples = 50
+    #length of signal
+    acqlen = 1000
+    #size of the dictionary - used for setting array size, can overestimate
+    no_entrys = 94500
+    #number of dimensions in the dictioanry
+    parameters = 7
+
+    #Name of the dictioanry
+    dictionaryid = 'FISP_WEX_ISMRM'
+    sampleid = dictionaryid
+
+    #load in dictionary 
+    #Load files
+    array = np.zeros((acqlen,no_entrys))
+
+    count = 0
+    sample = [0,0]
+    #rr = '*' +  str(repeat) + '.npy'
+    rr = '*_1.npy'
+    #Loading all signals
+
+    res_ave = np.zeros([noi,parameters])
+    res_std = np.zeros([noi,parameters])
+    res_est = np.zeros([parameters,samples])
+
+    dictionary, lookup = load_dictionary(dictionaryid, acqlen)
+
+    ## DEFINITION OF VARIATIONS FOR SIMULATION
+    # Specify the ranges and step sizes of the dictionary dimensions
+    # intravascular water residence time (res) UNIT: ms
+    resArray = range(200,1700,100) #range(200,1700,107) #range(200,1700,70) 
+    # percentage blood volume (perc) UNIT: %
+    percArray = range(10,110,10) #REMEMBER IT WILL BE DIVIDED BY 10 110
+    #T1 of tissue compartment (t1t) UNIT: ms
+    t1tArray = range(1000,2200,200) #range(700,1700,69) 
+    #T1 of blood compartment (t1b) UNIT: ms
+    t1bArray = range(1500,2100,200) #range(1540,1940,27) 
+    # multiplication factor for the B1 value (multi)
+    multiArray = range(80, 130, 10) #100
+    # T2 of tissue compartment UNIT: ms
+    t2tArray = range(38,127,15)
+    # T2 of blood compartment UNIT: ms
+    t2bArray = range(55, 385, 110)
+    if t2tArray[-1] > 112:
+        t2tArray= list(t2tArray)
+        t2tArray[-1] = 112
+    if percArray[-1] > 100:
+        percArray= list(percArray)
+        percArray[-1] = 100
+    
+    # find mean T1 tissue and T1 blood
+    t1tmean = np.mean(t1tArray)
+    t1bmean = np.mean(t1bArray)
+    t2tmean = np.mean(t2tArray)
+    t2bmean = np.mean(t2bArray)
+
+    # remove params from the search space:
+    # if T1 tissue is above the mean, T2 tissue below the mean should be removed from list of params in itertools
+
+
+    # to iterate through all possible simulation values
+    params = list(itertools.product(t1tArray, t1bArray, t2tArray, t2bArray, resArray, percArray, multiArray))
+    filtered_combinations = [
+    (t1t, t1b, t2t, t2b, res, perc, multi) for t1t, t1b, t2t, t2b, res, perc, multi in params
+    if not (t1t > t1tmean and t2t < t2tmean) and not(t1b > t1bmean and t2b < t2bmean) and not (t2t > t2tmean and t1t < t1tmean) and not (t2b > t2bmean and t1b < t1bmean) 
+    ]
+
+
+##########################
+    otherParams = list([res_ave,res_std, res_est, sampleid, dictionary,lookup, acqlen, samples])
+    # Verify the shape of params
+
+    otherParams = np.tile(np.array(otherParams, dtype=object),[np.size(params,0),1])
+    allParams = list(np.append(np.array(params), otherParams, axis=1)) 
+    
+    print('Beginning matching...')
+
+    if platform.system() == "Darwin":
+        #If on local computer can use all CPUs
+        pool = mp.Pool(12) #12
+    else:
+        #If on cluster only use a few 
+        pool = mp.Pool(8)
+
+    #Start timer
+    t0 = time.time()
+
+    #Run main function in parallel 
+    #Current laptop (2021 M1 Macbook Pro) will have 8 CPUs available
+    try:
+        pool.map(process_matching, allParams)
+    finally:
+        #Terminate and join the threads after parallelisation is done
+        pool.terminate()
+        pool.join()
+        pool.close()
+
+    #Stop timer and print                                                    
+    t1 = time.time()
+    total = t1-t0
+    print(total)   
+
+
+
+"""
+
+
+    -----------------------MATCHING--------------------------------
+    for per in [10]: #range(10,105,10): #range(5,105,5): #range(1,11,1): 
+        for res in [200]: #range(200,1700,100): #range(200,1700,100): #100000000000
+            for T1t in [1000]: #[1300]: #range(600,1600,100): #range(1100,1550,50):
+                for T1b in [1500]: #[1700]: #range(1500,2100,200):
+                    for multi in [80]: #[100]: #range(80, 120, 10): #range(80, 125, 5):
+                        res_est = np.zeros([parameters,samples])                                  
+                        for noise in range(0,noi):
+                                #for samp in range(0,samples):      
+                                samp=49
+                                correct_array = []
+                                ind =[]
+                                
+                                #perc = sam[int(per)]
+                                perc = per/10
+                            
+                                #Load the signal to match 
+                                test_name = './dictionaries/Dictionary' + sampleid + '/echo_' + str(T1t) + '_' + str(T1b) + '_' + str(res) + '_' + str(perc) + '_' + str(multi/100) + '_' + str(samp+1) + '.npy' 
+
+                                hold =  np.load(test_name)
+                                #test_signal = hold[:acqlen,noise]
+                                test_signal = hold[:acqlen,noise, samples]
+                                test_signal = test_signal/np.linalg.norm(test_signal)
+                            
+                                #test_signal = np.expand_dims(test_signal, axis=0)
+                                #Calculate the dot product 
+
+                                dot_sum = np.matmul(test_signal,dictionary[:acqlen,:])
+                                print(dot_sum)
+                                #Find max index 
+                                dot_sum = dot_sum[dot_sum != 0]
+                                
+                                max_index = np.argmax(dot_sum)
+                                ind.append(max_index)
+                                match_name = files[max_index]
+                                strsplit = match_name.split('_')
+                                #try:
+                                samp = 1
+                                res_est[:,samp] = [float(strsplit[2]),float(strsplit[3]), float(strsplit[4]), float(strsplit[5]), float(strsplit[6])]
+                                    #res_est[:,samp] = [float(strsplit[2]),float(strsplit[3]), float(strsplit[4]), float(strsplit[5]), float(strsplit[6])] #[:-4] 
+                                #except: 
+                                    #res_est[:,samp] = [float(strsplit[1]),float(strsplit[2]), float(strsplit[3]), float(strsplit[4]), float(strsplit[5])] #[:-4] 
+                    
+                                #mode_calc, count = mode(res_est[:,:-1], axis=1)
+                                #res_ave[noise,:] = np.squeeze(mode_calc)
+                                res_ave[noise,:] = np.median(res_est[:,1:], axis=1)
+                                res_std[noise,:] = iqr(res_est[:,1:], axis=1)
+                        #res_perc[noise,:] = (np.mean(res_est, axis=1)-res)/res*100
+                        
+                        print('Input signal')
+                        print(test_name)  
+                        print ('Matched:')
+                        print(str(res_ave[0,0]), str(res_ave[0,1]),
+                            str(res_ave[0,2]), str(res_ave[0,3]), str(res_ave[0,4]))
+                    
+                        pc = [res_ave, res_std]
+                        
+
+                        file_name = './dictionaries/SimulationMatching/Matching'+ sampleid + '/' + str(T1t) + '_' + str(T1b) + '_' + str(res) + '_' + str(perc) + '_' + str(multi/100) + '_pc.npy'
+        
+                        np.save(file_name, pc)
+"""
